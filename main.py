96b349008154510cfebcb4a691d7650d35518c77
cocoquiet <cocoquiet@knu.ac.kr>
2023-10-13 05:40:26 +0900

    Feat: 학생 미인증 경우 추가(deny_unknown_student)

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pyzbar.pyzbar as pyzbar
 from collections import deque
 import winsound as ws
 import time
+import pandas as pd
+
+import sys
 
 
 def confirm_student(student_id, scanned_id_list) -> None:
@@ -38,9 +41,29 @@ def deny_overlap_student(student_id) -> None:
     print(f'학번 : {student_id}')
     print('이미 배부받은 학생입니다.')
     print('----------------------------------\n')
+    
+def deny_unknown_student(student_id) -> None:
+    """학생 미인증 거부 함수
+
+    Args:
+        student_id (int): 학생의 학번
+    """
+    
+    ws.Beep(1500, 400)
+    
+    print(f'학번 : {student_id}')
+    print('미등록 학생입니다.')
+    print('----------------------------------\n')
 
 
 if __name__ == '__main__':
+    db_path = '../student_list.csv'
+    
+    db = pd.read_csv(db_path, usecols=[1, 2, 3])\
+            .dropna(subset=['student_id'])
+            
+    student_id_list = db['student_id'].tolist()
+    
     cap = cv2.VideoCapture(0)
     scanned_id_list = deque()
     scan_time = 0
@@ -68,13 +91,17 @@ if __name__ == '__main__':
         for obj in decodedObjects:
             if obj.type == 'QRCODE':
                 student_id = obj.data.decode('utf-8')[:10]
-                if student_id not in scanned_id_list:
+                if student_id not in scanned_id_list and student_id in student_id_list:
                     scan_time = time.time()
                     confirm_student(student_id, scanned_id_list)
                     
                 elif student_id in scanned_id_list and time.time() - scan_time > 3:
                     scan_time = time.time()
                     deny_overlap_student(student_id)
+                    
+                elif student_id not in student_id_list and time.time() - scan_time > 3:
+                    scan_time = time.time()
+                    deny_unknown_student(student_id)
         
         if cv2.getWindowProperty('QR Code Scanner', cv2.WND_PROP_VISIBLE) < 1:
             break
